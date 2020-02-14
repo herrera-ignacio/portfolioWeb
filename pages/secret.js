708c69be513f444328b19ca0a759df36d5c46841
Ignacio Roman Herrera <ignacioromanherrera@gmail.com>
2020-02-13 21:52:07 -0300

    Add secret endpoint to be consumed from secret page

--- pages/secret.js
+++ pages/secret.js
@@ -2,6 +2,7 @@ import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
 import withAuth from '../components/hoc/withAuth';
+import axios from 'axios';
 
 class Secret extends React.Component {
 	static async getInitialProps() {
@@ -9,15 +10,50 @@ class Secret extends React.Component {
 		return { superSecretValue };
 	}
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			secretData: []
+		}
+	}
+
+	async componentDidMount() {
+		const res = await axios.get('/api/v1/secret');
+		const secretData = res.data;
+
+		this.setState({
+			secretData,
+		});
+	}
+
+	displaySecretData() {
+		const { secretData } = this.state;
+
+		if (secretData && secretData.length > 0) {
+			return secretData.map((data, index) => {
+				return (
+					<div key={index}>
+						<p> { data.title } </p>
+						<p> { data.description } </p>
+					</div>
+				)
+			});
+		}
+
+		return null;
+	}
+
 	render() {
 		const { superSecretValue } = this.props;
-		console.log(this.props);
+		console.log(this.state);
 		return (
 			<BaseLayout {...this.props.auth}>
 				<BasePage>
 					<h1>I am Secret Page</h1>
 					<p>Secret content here...</p>
 					<h2>{superSecretValue}</h2>
+					{ this.displaySecretData() }
 				</BasePage>
 			</BaseLayout>
 		)
